b2f8cc152d1e6b27595c574f1248d07a3dbd0648
Ryan McIntire <ryanmcin@gmail.com>
2016-03-15 11:21:37 -0700

    No tag.

--- github_test_script.sh
+++ github_test_script.sh
@@ -2,19 +2,19 @@
 
 branch="beta"
 master="master"
-backup="beta_bak"
-
-echo "Creating backup..."
-git checkout -B beta_bak
-git push origin $backup:$backup
 
 echo "Deleting $branch branch"
 git checkout master
 git branch -D $branch
 git push origin --delete $branch
 
-echo "Creating branch $branch"
-git checkout -b $branch
+echo "Geting most recent alpha release"
+git fetch upstream +refs/tags/*:refs/tags/remote/*
+recent_tag=$(git describe --bbrev=0)
+echo "Most recent tag: $recent_tag"
+
+echo "Creating branch $branch from "
+git checkout -B $branch $recent_tag
 git push origin $branch:$branch
 git checkout master
 
